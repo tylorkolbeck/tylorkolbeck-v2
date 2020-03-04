260616821c3d75d3e56650c1f71c1d649218e095
tylor.kolbeck@gmail.com <tylor.kolbeck@gmail.com>
2020-03-03 20:06:35 -0800

    updated resume

--- src/Components/Resume/Resume.js
+++ src/Components/Resume/Resume.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Resume() {
     return (
         <div className="Resume__container">
-            <object data='https://tylorkolbeck.s3-us-west-2.amazonaws.com/images/resume2019.pdf' type='application/pdf' width='100%' height='100%' style={{ background: 'rgb(82,86,89)'}}>
+            <object data='https://tylorkolbeck.s3-us-west-2.amazonaws.com/images/tylorkolbeck_resume2020.pdf' type='application/pdf' width='100%' height='100%' style={{ background: 'rgb(82,86,89)'}}>
                 <p>Failed to load the resume.</p>
             </object>
         </div>
