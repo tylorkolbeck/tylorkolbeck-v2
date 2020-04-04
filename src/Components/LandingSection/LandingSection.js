c4cabfb1a8d684081822eabcbd6482bb80494890
tylor.kolbeck@gmail.com <tylor.kolbeck@gmail.com>
2020-04-04 10:15:53 -0700

    remove background image from inline rule

--- src/Components/LandingSection/LandingSection.js
+++ src/Components/LandingSection/LandingSection.js
@@ -1,30 +1,31 @@
-import React from 'react'
-import './LandingSection.scss'
+import React from "react"
+import "./LandingSection.scss"
 
-import ScrollAnimation from 'react-animate-on-scroll'
-import { useHistory } from 'react-router-dom'
+import ScrollAnimation from "react-animate-on-scroll"
+import { useHistory } from "react-router-dom"
 // import HeaderImage from '../../assets/images/LandingSectionImage1.jpg'
-import ImageOverlay from '../UI/ImageOverlay/ImageOverlay'
-import Button from '../Button/Button'
+import ImageOverlay from "../UI/ImageOverlay/ImageOverlay"
+import Button from "../Button/Button"
 
 function LandingSection({ introText }) {
-    let history = useHistory()
-    return (
-        <div className="LandingSection__container" style={{ backgroundImage: `url(https://tylorkolbeck.s3-us-west-2.amazonaws.com/images/LandingSectionImage1.jpg)` }}>
-            <div>
-                <ScrollAnimation animateIn="fadeInLeft" animateOnce>
-                    <div className="LandingSection__content-header">
-                        {introText}
-                    </div>
-                </ScrollAnimation>
-                <div className="LandingSection__actionButtons">
-                    <Button text='View Resume' onClick={() => history.push('/resume')} type='btn-primary' />
-                </div>
-            </div>
-            <ImageOverlay opacity={.5} />
-
+  let history = useHistory()
+  return (
+    <div className="LandingSection__container">
+      <div>
+        <ScrollAnimation animateIn="fadeInLeft" animateOnce>
+          <div className="LandingSection__content-header">{introText}</div>
+        </ScrollAnimation>
+        <div className="LandingSection__actionButtons">
+          <Button
+            text="View Resume"
+            onClick={() => history.push("/resume")}
+            type="btn-primary"
+          />
         </div>
-    )
+      </div>
+      <ImageOverlay opacity={0.5} />
+    </div>
+  )
 }
 
 export default LandingSection
